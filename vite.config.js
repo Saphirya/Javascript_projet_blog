f6876dcc27985325ce8903b229bd469f57019393
Florian <floriangeorges1992@gmail.com>
2024-12-09 21:21:36 +0100

    init repo blog

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src", // Chemin vers le dossier contenant index.html
+
+  build: {
+    outDir: "../dist", // Chemin vers le dossier de sortie
+  },
+
+  // Chemin vers le dossier de sortie
+});
