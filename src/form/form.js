bd973ae07c4db5149b93f17767813806450a2771
Florian <floriangeorges1992@gmail.com>
2024-12-11 15:20:57 +0100

    add form

--- src/form/form.js
+++ src/form/form.js
@@ -1,5 +1,16 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
 
-import "/assets/javascripts/topbar.js";
-console.log("in form");
+//recuperation du formulaire
+const form = document.querySelector("form");
+let errors = [];
+//On ecoute le submit du formulaire
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //creation du formData
+  const formData = new FormData(form);
+  console.log(formData);
+  //transformer en objet javascript et en json
+  const json = JSON.stringify(Object.fromEntries(formData.entries()));
+  console.log(json);
+});
