ba96adf69ff68bb43561adb8ccd174497fd1a987
Florian <floriangeorges1992@gmail.com>
2024-12-18 10:44:49 +0100

    add dynamic menu

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "./assets/styles/styles.scss";
 import "./index.scss";
 
 const articleContainerElement = document.querySelector(".articles-container");
+const categoriesContainerElement = document.querySelector(".categories");
+
 const createArticles = (articles) => {
   const articlesDom = articles.map((article) => {
     const articleDom = document.createElement("div");
@@ -73,6 +75,35 @@ const createArticles = (articles) => {
     });
   });
 };
+const displayMenuCategories = (categoriesArr) => {
+  const liElements = categoriesArr.map((categoryElem) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${categoryElem[0]} <strong>(${categoryElem[1]})</strong>`;
+    return li;
+  });
+  categoriesContainerElement.innerHTML = "";
+  categoriesContainerElement.append(...liElements);
+  console.log(liElements);
+};
+
+//utilisaton de reduce pour créer le menu catégories
+const createMenuCategories = (articles) => {
+  const categories = articles.reduce((acc, article) => {
+    if (acc[article.category]) {
+      acc[article.category]++;
+      return acc;
+    } else {
+      acc[article.category] = 1;
+      return acc;
+    }
+  }, {});
+
+  const categoriesArr = Object.keys(categories).map((category) => {
+    return [category, categories[category]];
+  });
+  displayMenuCategories(categoriesArr);
+  console.log(categoriesArr);
+};
 
 //requete GET
 const fetchArticles = async () => {
@@ -80,13 +111,12 @@ const fetchArticles = async () => {
     const response = await fetch("https://restapi.fr/api/article");
     let articles = await response.json();
     console.log("Données reçues :", articles);
-
     // Transformez en tableau si un seul article est retourné
     if (!Array.isArray(articles)) {
       articles = [articles];
     }
-
     createArticles(articles);
+    createMenuCategories(articles);
   } catch (e) {
     console.error("Erreur dans fetchArticles :", e);
   }
